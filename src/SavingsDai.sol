b1f2857b7f864eb8d406da580a6a7eb60be731f6
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2019-08-13 15:20:04 -0600

    initial ERC sDai and join adapter

--- src/SavingsDai.sol
+++ src/SavingsDai.sol
@@ -1,4 +1,110 @@
 pragma solidity ^0.5.10;
 
-contract SavingsDai {
+import "../lib/dss/src/lib.sol";
+
+contract SavingsDai is DSNote {
+        // --- Auth ---
+    mapping (address => uint) public wards;
+    function rely(address guy) external note auth { wards[guy] = 1; }
+    function deny(address guy) external note auth { wards[guy] = 0; }
+    modifier auth { require(wards[msg.sender] == 1); _; }
+
+    // --- ERC20 Data ---
+    string  public constant name     = "Savings Dai";
+    string  public constant symbol   = "sDAI";
+    string  public constant version  = "1";
+    uint8   public constant decimals = 18;
+    uint256 public totalSupply;
+
+    mapping (address => uint)                      public balanceOf;
+    mapping (address => mapping (address => uint)) public allowance;
+    mapping (address => uint)                      public nonces;
+
+    event Approval(address indexed src, address indexed guy, uint wad);
+    event Transfer(address indexed src, address indexed dst, uint wad);
+
+    // --- Math ---
+    function add(uint x, uint y) internal pure returns (uint z) {
+        require((z = x + y) >= x);
+    }
+    function sub(uint x, uint y) internal pure returns (uint z) {
+        require((z = x - y) <= x);
+    }
+
+    // --- EIP712 niceties ---
+    bytes32 public DOMAIN_SEPARATOR;
+    bytes32 public constant PERMIT_TYPEHASH = keccak256(
+        "Permit(address holder,address spender,uint256 nonce,uint256 expiry,bool allowed)"
+    );
+
+    constructor(uint256 chainId_) public {
+        wards[msg.sender] = 1;
+        DOMAIN_SEPARATOR = keccak256(abi.encode(
+            keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+            keccak256(bytes(name)),
+            keccak256(bytes(version)),
+            chainId_,
+            address(this)
+        ));
+    }
+
+    // --- Token ---
+    function transfer(address dst, uint wad) external returns (bool) {
+        return transferFrom(msg.sender, dst, wad);
+    }
+    function transferFrom(address src, address dst, uint wad)
+        public returns (bool)
+    {
+        require(balanceOf[src] >= wad, "dai/insufficient-balance");
+        if (src != msg.sender && allowance[src][msg.sender] != uint(-1)) {
+            require(allowance[src][msg.sender] >= wad, "dai/insufficient-allowance");
+            allowance[src][msg.sender] = sub(allowance[src][msg.sender], wad);
+        }
+        balanceOf[src] = sub(balanceOf[src], wad);
+        balanceOf[dst] = add(balanceOf[dst], wad);
+        emit Transfer(src, dst, wad);
+        return true;
+    }
+    function mint(address usr, uint wad) external auth {
+        balanceOf[usr] = add(balanceOf[usr], wad);
+        totalSupply    = add(totalSupply, wad);
+        emit Transfer(address(0), usr, wad);
+    }
+    function burn(address usr, uint wad) external {
+        require(balanceOf[usr] >= wad, "dai/insufficient-balance");
+        if (usr != msg.sender && allowance[usr][msg.sender] != uint(-1)) {
+            require(allowance[usr][msg.sender] >= wad, "dai/insufficient-allowance");
+            allowance[usr][msg.sender] = sub(allowance[usr][msg.sender], wad);
+        }
+        balanceOf[usr] = sub(balanceOf[usr], wad);
+        totalSupply    = sub(totalSupply, wad);
+        emit Transfer(usr, address(0), wad);
+    }
+    function approve(address usr, uint wad) external returns (bool) {
+        allowance[msg.sender][usr] = wad;
+        emit Approval(msg.sender, usr, wad);
+        return true;
+    }
+    // --- Approve by signature ---
+    function permit(address holder, address spender, uint256 nonce, uint256 expiry,
+                    bool allowed, uint8 v, bytes32 r, bytes32 s) external
+    {
+        bytes32 digest =
+            keccak256(abi.encodePacked(
+                "\x19\x01",
+                DOMAIN_SEPARATOR,
+                keccak256(abi.encode(PERMIT_TYPEHASH,
+                                     holder,
+                                     spender,
+                                     nonce,
+                                     expiry,
+                                     allowed))
+        ));
+        require(holder == ecrecover(digest, v, r, s), "dai/invalid-permit");
+        require(expiry == 0 || now <= expiry, "dai/permit-expired");
+        require(nonce == nonces[holder]++, "dai/invalid-nonce");
+        uint wad = allowed ? uint(-1) : 0;
+        allowance[holder][spender] = wad;
+        emit Approval(holder, spender, wad);
+    }
 }
---
+++ src/join.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.5.10;
+
+import "../lib/dss/src/lib.sol";
+
+contract DSTokenLike {
+    function mint(address,uint) external;
+    function burn(address,uint) external;
+}
+
+contract PotLike {
+    mapping(address => mapping (address => uint)) public can;
+    function move(address, address, uint) external;
+}
+
+contract sDaiJoin is DSNote{
+    PotLike public pot;
+    DSTokenLike public sDai;
+    constructor(address pot_, address sDai_) public {
+        pot = PotLike(pot_);
+        sDai = DSTokenLike(sDai_);
+    }
+    uint constant ONE = 10 ** 27;
+    function mul(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x);
+    }
+    function join(address usr, uint wad) external note {
+        pot.move(msg.sender, address(this), mul(ONE, wad));
+        sDai.mint(usr, wad);
+    }
+    function exit(address usr, uint wad) external note {
+        sDai.burn(msg.sender, wad);
+        pot.move(address(this), usr, mul(ONE, wad));
+    }
+}
